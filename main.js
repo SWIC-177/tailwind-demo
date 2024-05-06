38b2a3ac571411c48d9733fa040e4bd0c7ef3d6f
Manav Misra <manavm@visionwebsoft.com>
2024-05-05 20:44:04 -0500

    'Select' the submit button

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
 ];
+
+// What if we had other buttons like a 'reset' button?
+const submitBtn = document.querySelector("button[type='submit']");
